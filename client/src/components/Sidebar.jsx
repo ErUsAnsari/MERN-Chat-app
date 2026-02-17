0c3e7f3b6ba62fa02e3709ab9cb60fd3fed4b8e9
Usman Ansari <usmanansari2688@gmail.com>
2026-02-17 13:21:21 +0530

    02 Sidebar Design

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import assets from "../assets/assets";
+import assets, { userDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({ selectedUser, setSelectedUser }) => {
   const navigate = useNavigate();
   return (
     <div
-      className={`bg-[#8185B2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${
+      className={`backdrop-blur-xl border shadow-2xl bg-[#F8FAFC] border-r border-solid border-[#E2E8F0]  h-full p-5 rounded-r-xl overflow-y-scroll text-[#0F172A] ${
         selectedUser ? "max-md:hidden" : ""
       }`}
     >
@@ -36,6 +36,46 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
             </div>
           </div>
         </div>
+        <div className="bg-[#FFFFFF] border border-gray-500 rounded-full flex items-center gap-2 py-3 px-4 mt-5">
+          <img src={assets.search_icon} alt="search" className="w-3" />
+          <input
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            className="bg-[#FFFFFF] focus:ring-4 focus:ring-blue-600/10 border-slate-200 outline-none border-1px border-solid text-[#0F172A] text-xs placeholder-[#0F172A] flex-1"
+            placeholder="Search User..."
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        {userDummyData.map((user, index) => (
+          <div
+            onClick={() => setSelectedUser(user)}
+            key={index}
+            className={`text-sm font-bold text-slate-800 transition-colors cursor-pointer p-3 rounded-xl mx-2 relative flex items-center gap-2 pl-4 max-sm:text-sm hover:bg-[#EFF6FF] ${
+              selectedUser?._id === user._id && "bg-[#282142]/50"
+            }`}
+          >
+            <img
+              src={user?.profilePic || assets.avatar_icon}
+              alt=""
+              className="w-8.75 aspect-square rounded-full"
+            />
+            <div className="flex flex-col leading-5">
+              <p>{user.fullName}</p>
+              {index < 3 ? (
+                <span className="text-[#22C55E] text-xs">Online</span>
+              ) : (
+                <span className="text-[#94A3B8] text-xs">Offline</span>
+              )}
+            </div>
+            {index > 2 && (
+              <p className="bg-linear-to-tr from-blue-600 to-blue-400 text-[10px] h-5 w-5 flex items-center justify-center rounded-full text-white font-bold absolute top-4 right-4 text-xs bg-[#2563EB]">
+                {index}
+              </p>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
