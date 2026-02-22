abbb7d4d628620b92b44d472cd7706a5344e136e
Usman Ansari <usmanansari2688@gmail.com>
2026-02-22 16:21:47 +0530

    18 Login and Logout Context for authentication and socket connection

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -21,15 +21,59 @@ export const AuthProvider = ({ children }) => {
       const { data } = await axios.get("/api/auth/check");
       if (data.success) {
         setAuthUser(data.user);
+        connectSocket(data.user);
       }
     } catch (error) {
       toast.error(error.message);
     }
   };
 
+  // Login function to handle user authentication and socket connection
+  const login = async (state, credentials) => {
+    try {
+      const { data } = await axios.post(`/api/auth/${state}`, credentials);
+      if (data.success) {
+        setAuthUser(data.userData);
+        connectSocket(data.userData);
+        axios.defaults.headers.common["token"] = data.token;
+        setToken(data.token);
+        localStorage.setItem("token", data.token);
+        toast.success(data.message);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  // Login function to handle user authentication and socket connection
+  const logout = async () => {
+    localStorage.removeItem("token");
+    setToken(null);
+    setAuthUser(null);
+    setOnlineUsers([]);
+    axios.defaults.headers.common["token"] = null;
+    toast.success("Logged out Successfully");
+    socket.disconnect();
+  };
+
   // Connect socket function to handle socket connection and online user updates
   const connectSocket = (userData) => {
     if (!userData || socket?.connected) return;
+
+    const newSocket = io(backendUrl, {
+      query: {
+        userId: userData._id,
+      },
+    });
+
+    newSocket.connect();
+    setSocket(newSocket);
+
+    newSocket.on("getOnlineUsers", (userIds) => {
+      setOnlineUsers(userIds);
+    });
   };
 
   useEffect(() => {
