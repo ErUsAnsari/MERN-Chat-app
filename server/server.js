bd0f58bd0648069c17e17bd6c1f771853f23d3cd
Usman Ansari <usmanansari2688@gmail.com>
2026-02-26 03:48:54 +0530

    25 Server fix

--- server/server.js
+++ server/server.js
@@ -50,7 +50,13 @@ app.use("/api/messages", messageRouter)
 // Connect to MongoDB
 await connectDB()
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-    console.log("Server is Running on PORT : " + PORT);
-})
+
+if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+        console.log("Server is Running on PORT : " + PORT);
+    })
+}
+
+// exporting server for Vercel
+export default server;
